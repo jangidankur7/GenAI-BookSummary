9724ecfdb283ce222cecd313d92d25575da299c9
root <root@DESKTOP-N61QAPK>
2024-10-24 17:54:11 +0530

    remove postgres uri hardcode

--- my-app/db/utils.py
+++ my-app/db/utils.py
@@ -5,10 +5,8 @@ from typing import Annotated
 from fastapi import Depends
 import logging
 
-# dotenv.load_dotenv('/root/GenAI-BookSummary/my-app/.env')
-# POSTGRES_URI = os.getenv('POSTGRES_URI')
-
-POSTGRES_URI='postgresql://avnadmin:AVNS_IzoUvCF2an6Q-ro89o6@postgres-aiven-officialaviid-2c3c.i.aivencloud.com:17942/defaultdb?sslmode=require'
+dotenv.load_dotenv('/root/GenAI-BookSummary/my-app/.env')
+POSTGRES_URI = os.getenv('POSTGRES_URI')
 
 engine = create_engine(POSTGRES_URI)
 # engine = create_engine(sqlite_url, connect_args=connect_args)
